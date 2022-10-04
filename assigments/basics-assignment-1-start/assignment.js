539d5e9b48bb54783811fd580f8a94906eaa0ffe
andonirdgz <andoni_rdgz@outlook.com>
2022-10-04 08:19:24 -0700

    First assigment completed

---
+++ assigments/basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+let userInput = 2
+let result
+
+result = 18 + userInput
+
+result = result - 2
+result = result * 2
+result = result / 3
+
+alert(`Input: ${userInput}`)
+alert(`Result: ${result}`)
