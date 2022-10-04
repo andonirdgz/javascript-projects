968d67f862f9e137e3d83874555512dec7b8613c
andonirdgz <andoni_rdgz@outlook.com>
2022-10-04 08:41:06 -0700

    Assigment 2 - solved

--- assigments/basics-assignment-2-start/assignment.js
+++ assigments/basics-assignment-2-start/assignment.js
@@ -1,1 +1,23 @@
-const task3Element = document.getElementById('task-3');
+const task3Element = document.getElementById('task-3')
+
+function showAlert() {
+  alert('No parameters')
+}
+
+function showNameAlert(name) {
+  alert(`Hi ${name}!`)
+}
+
+// showAlert()
+// showNameAlert('Andoni')
+
+task3Element.addEventListener('click', showAlert)
+
+function showProfileAlert(name, lastName, age) {
+  alert(`Profile
+  Name: ${name}
+  Last Name: ${lastName}
+  Age: ${age}`)
+}
+
+showProfileAlert('Andoni', 'Rodriguez', '22')
